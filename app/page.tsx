95674587ea70efbc9238a873f809ac519610a3a9
wesdxc1717-art <wesdxc1717@gmail.com>
2026-08-17 18:34:38 +0900

    Fix form layout and amount input

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import InfoSection from "@/components/InfoSection"; // 1. 유의사항 컴포넌트 불러오기
 import BottomForm from "@/components/BottomForm";
 
 export default function Home() {
@@ -7,6 +8,10 @@ export default function Home() {
     <main className="min-h-screen bg-white pb-32">
       <Header />
       <Hero />
+      
+      {/* 2. 대출 안내 및 유의사항 섹션 추가 */}
+      <InfoSection />
+
       <div id="bottom-form">
         <BottomForm />
       </div>
